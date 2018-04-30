e2aace84909d8a63d6217af345b4100ffb53e289
Rebekah Van Dyke <rebekah.vandyke@pcc.edu>
2018-04-29 21:29:15 -0700

    added main method printing hello world

--- src/pcc/cis/helloworld/helloWorld.java
+++ src/pcc/cis/helloworld/helloWorld.java
@@ -1,4 +1,13 @@
+/**
+ * Simple "Hello World" Program
+ * Author Rebekah Van Dyke
+ * Date 4/29/2018
+ */
+
 package pcc.cis.helloworld;
 
 public class helloWorld {
+    public void main() {
+        System.out.println("Hello World");
+    }
 }
